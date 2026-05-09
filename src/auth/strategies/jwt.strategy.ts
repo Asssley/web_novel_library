e454231426019ff5e214b52f6be89dfbcd466680
Nazar Turbin <turbinnazar@gmail.com>
2026-05-09 20:25:14 +0200

    add strategy name to JwtStrategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -6,7 +6,7 @@ import { Injectable } from "@nestjs/common";
 import { Request } from "express";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
   constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
