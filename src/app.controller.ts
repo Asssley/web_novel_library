a6863facbbb1e9f4b9a9bc9c3aad3a2f70137b2e
Nazar Turbin <turbinnazar@gmail.com>
2026-05-24 23:30:06 +0200

    add rendering of main page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service.js';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-  ) {}
+  ) { }
 
   @Get()
+  @Render("pages/home")
   async getHomePage() {
-    return this.appService.getHomePage();
+    const data = await this.appService.getHomePage()
+
+    return {
+      ...data,
+      title: "NovelsHere",
+      styles: [
+        "pages/home.css"
+      ],
+      scripts: []
+    };
   }
 }
