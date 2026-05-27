48ce3aa336dc36cfa24ba131de509f128ef9153b
Nazar Turbin <turbinnazar@gmail.com>
2026-05-27 16:02:50 +0200

    bug fix

--- src/chapter/chapter.module.ts
+++ src/chapter/chapter.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ChapterService } from './chapter.service.js';
 import { ChapterController as ChapterApiController } from './api/chapter.controller.js';
-import { ChapterController as ChapterViewController} from './api/chapter.controller.js';
+import { ChapterController as ChapterViewController} from './view/chapter.controller.js';
 
 @Module({
   controllers: [
